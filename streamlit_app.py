a154d1196bc3f1f50452e1ff14c23f7ae715ea1e
Jiyae <mjy3056@naver.com>
2022-11-08 23:49:27 +0900

    u

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import joblib
 import numpy as np
 
 # 헤드라인
-st.write("# 보험료 예측")
+st.write("## 보험료 예측하기")
+st.image("https://www.accuquote.com/wp-content/uploads/2019/08/hands-in-hands-protecting-paper-family.jpg",caption='insurance')
 
 # 첫번째 행
 r1_col1, r1_col2, r1_col3 = st.columns(3)
@@ -37,12 +38,6 @@ is_northwest = region_option[2] == region
 
 # 예측 버튼
 predict_button = st.button("예측")
-########################################연습
-# r3_col1, r3_col2 = st.columns(2)
-# g = ('Comedy', 'Drama', 'Documentary')
-# genre = r3_col1.radio("What's your favorite movie genre", g)
-# genre[0]
-
 st.write("---")
 
 # 예측 결과

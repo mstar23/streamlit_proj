bc92b0b182ade04e5c0491cc33c929505c3fdbfa
Jiyae <mjy3056@naver.com>
2022-11-08 23:31:33 +0900

    u

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,17 +25,20 @@ sex = r2_col2.selectbox("sex", sex_option)
 is_male = sex_option[0] == sex
 
 region_option = ('southwest', 'southeast', 'northwest', 'northeast')
-#region = r2_col3.selectbox("region", region_option)
-#is_southwest = region_option[0] == region
-#is_southeast = region_option[1] == region
-#is_northwest = region_option[2] == region
-region = r2_col3.radio(region_option)
+region = r2_col3.selectbox("region", region_option)
 is_southwest = region_option[0] == region
 is_southeast = region_option[1] == region
 is_northwest = region_option[2] == region
+
+# region = r2_col3.radio(region_option)
+# is_southwest = region_option[0] == region
+# is_southeast = region_option[1] == region
+# is_northwest = region_option[2] == region
 # 예측 버튼
 predict_button = st.button("예측")
-
+genre = st.radio(
+    "What's your favorite movie genre",
+    ('Comedy', 'Drama', 'Documentary'))
 st.write("---")
 
 # 예측 결과

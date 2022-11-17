a55404372773e98ce4af241e8675756f6ea9e9c6
Mstar <mjy3056@naver.com>
2022-11-18 01:20:14 +0900

    check

--- streamlit_prac1/prac1_app.py
+++ streamlit_prac1/prac1_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import joblib
+# import joblib
 import pandas as pd # 판다스 불러오기
 
 from PIL import Image # 파이썬 기본라이브러리는 바로 사용 가능!
@@ -13,12 +13,11 @@ def get_image(image_name):
 st.write('# Prac1 실습 페이지')
 #### 데이터 불러오기 ####
 st.write('#### 전처리한 데이터')
-pkl_path = f"{os.path.dirname(os.path.abspath(__file__))}/lgbm_df.pkl"
-lgbm_df = joblib.load(pkl_path)
-lgbm_df = pd.DataFrame(lgbm_df)
-st.write(lgbm_df.head())
+st.write('왜 오류?????')
+# lgbm_df = joblib.load('lgbm_df.pkl')
+# st.write(lgbm_df.head())
 
 ### 불러온 데이터로 훈련-테스트셋 분리
 
 ## joblib 설치 했는데 실행하는법 + 대용량 pkl 파일 guthub 업로드 방법???
-
+## 

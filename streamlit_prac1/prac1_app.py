190cc741da5a845f1d7257ee6a97e14e92b167aa
Mstar <mjy3056@naver.com>
2022-11-18 00:17:04 +0900

    pkl commit

--- streamlit_prac1/prac1_app.py
+++ streamlit_prac1/prac1_app.py
@@ -15,6 +15,7 @@ st.write('# Prac1 실습 페이지')
 st.write('#### 전처리한 데이터')
 pkl_path = f"{os.path.dirname(os.path.abspath(__file__))}/lgbm_df.pkl"
 lgbm_df = joblib.load(pkl_path)
+lgbm_df = pd.DataFrame(lgbm_df)
 st.write(lgbm_df.head())
 
 ### 불러온 데이터로 훈련-테스트셋 분리

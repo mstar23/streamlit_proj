f806793dfd9f6ca05a9bdb639f4f71ed10612e7c
Mstar <mjy3056@naver.com>
2022-11-19 22:37:09 +0900

    df 불러오기

--- streamlit_prac1/prac1_app.py
+++ streamlit_prac1/prac1_app.py
@@ -10,7 +10,8 @@ st.write('여기에 lgbm_df 업로드 되어 있기 때문에 여기서 lgbm 실
 #### 데이터 불러오기 ####
 st.write('#### 전처리한 데이터')
 st.write('큰일 날뻔 ㅎ')
-# pkl_path = f"{os.path.dirname(os.path.abspath(__file__))}/lgbm_df.pkl"
-lgbm_df = joblib.load('streamlit_prac1\lgbm_df.pkl')
+pkl_path = f"{os.path.dirname(os.path.abspath(__file__))}/lgbm_df.pkl"
+# lgbm_df = joblib.load('streamlit_prac1\lgbm_df.pkl')
+lgbm_df = joblib.load(pkl_path)
 lgbm_df = pd.DataFrame(lgbm_df)
 st.write(lgbm_df.head())

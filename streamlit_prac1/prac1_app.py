c40db3f717013258e609038566d129fda0e0f749
Mstar <mjy3056@naver.com>
2022-11-19 22:51:56 +0900

    df 나중에 해결해보자

--- streamlit_prac1/prac1_app.py
+++ streamlit_prac1/prac1_app.py
@@ -10,9 +10,9 @@ st.write('---')
 
 #### 데이터 불러오기 ####
 st.write('#### 전처리한 데이터')
-st.write('아 왜 자꾸 파일을 찾을 수 없대!!!!! git - lfs 문제인듯 ')
-pkl_path = f"{os.path.dirname(os.path.abspath(__file__))}/lgbm_df.pkl"
-# lgbm_df = joblib.load('streamlit_prac1\lgbm_df.pkl')
-lgbm_df = joblib.load(pkl_path)
-lgbm_df = pd.DataFrame(lgbm_df)
-st.write(lgbm_df.head())
+st.write('아 왜 자꾸 파일을 찾을 수 없대!!!!! git - lfs 문제인듯 빨리 해결해..')
+# pkl_path = f"{os.path.dirname(os.path.abspath(__file__))}/lgbm_df.pkl"
+# # lgbm_df = joblib.load('streamlit_prac1\lgbm_df.pkl')
+# lgbm_df = joblib.load(pkl_path)
+# lgbm_df = pd.DataFrame(lgbm_df)
+# st.write(lgbm_df.head())
